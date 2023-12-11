e1e4492bc4dc853f35e5c3e91295d68f3cfba5e2
maxinth <tunde.j.adeleye@gmail.com>
2023-12-11 21:29:36 +0100

    updates to hero section

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,42 @@
+const RightArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="34"
+      height="34"
+      viewBox="0 0 34 34"
+      fill="none"
+    >
+      <g clipPath="url(#clip0_40_1679)">
+        <path
+          d="M9.89817 16.8686L24.0389 17.069"
+          stroke="#660000"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="group-hover:!stroke-[#fff]"
+        />
+        <path
+          d="M17.0687 9.89844L24.0389 17.069L16.8683 24.0392"
+          stroke="#660000"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="group-hover:stroke-[#fff]"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_40_1679">
+          <rect
+            width="24"
+            height="24"
+            fill="white"
+            transform="translate(17.2094) rotate(45.8119)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export { RightArrow };
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,9 @@ import { motion } from "framer-motion";
 import { useVariants } from "./useVariants";
 import Button from "./Button";
 import RotateDish from "../assets/rotate-dish.png";
+import { RightArrow } from "../assets/icons";
+import Highlight from "../assets/highlight.png";
+import ChefCap from "../assets/chefCap.png";
 
 const HeroSection = () => {
   const { variantProps, slideFromTop, pageVariant } = useVariants();
@@ -9,32 +12,46 @@ const HeroSection = () => {
     <motion.div
       variants={slideFromTop}
       {...variantProps}
-      className="bg-[#fff]  w-full h-[700px] bg-cover bg-no-repeat items-center  flex gap-4 px-8 py-10 pb-16"
+      className="bg-[#fff]  w-full h-[700px] bg-cover bg-no-repeat items-center  flex flex-col md:flex-row gap-4 px-8 py-10 pb-16"
     >
-      <motion.div variants={pageVariant(2, 1)} className="">
+      <motion.div variants={pageVariant(2, 1)} className="w-full px-4">
         <h1 className="text-4xl mb-3  font-Mulish font-light text-[#000]">
-          <span className="mb-[20px] inline-block"> Welcome to</span> <br />
-          <span className="font-[900] text-[#600] text-[72px] mb-[20px] inline-block">
+          <img
+            src={Highlight}
+            alt="highlight"
+            className="relative -left-8 top-4"
+          />
+          <span className="mb-[20px] inline-block !text-[20px] md:!text-[32px] text-center md:text-left uppercase ">
+            {" "}
+            Welcome to
+          </span>{" "}
+          <br />
+          <span className="font-[900] text-[#600] text-[45px] md:text-[72px] mb-[20px] inline-block">
             Yinkus Kitchen
           </span>
         </h1>
-        <p className="font-irishGrover text-[36px]  mb-[20px] ">
+        <p className="font-irishGrover md:text-[36px] text-[20px]  mb-[20px] ">
           A place where culinary magic happens!
         </p>
-        <h2 className="text-[20px] max-w-[430px] md:max-w-none mb-[32px]">
+        <h2 className="text-base md:text-[20px] max-w-[430px] md:max-w-none mb-[32px]">
           Discover a world of flavors in every bite, every dish tells a
           delicious story...
         </h2>
         <Button
           textColor={"#660000"}
           bgColor="#fff"
-          otherClasses="border border-[#660000] rounded-[30px]"
+          otherClasses="border duration-150 ease-in border-[#660000] hover:cursor-pointer rounded-[30px] flex items-center group gap-2 hover:!bg-[#660000] hover:!border-[#fff] hover:!text-[#fff]"
         >
-          Explore
+          Explore <RightArrow />
         </Button>
       </motion.div>
-      <div>
+      <div className="relative">
         <img src={RotateDish} alt="rotate dish" />
+        <img
+          src={ChefCap}
+          alt="cap"
+          className="absolute top-[25%] bottom-[10%] left-[25%] right-[90%}"
+        />
       </div>
     </motion.div>
   );
